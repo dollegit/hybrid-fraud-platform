f0070cb701f513bab085063e0f5f1577b94848f5
dollegit <dollegit@yahoo.com>
2025-12-10 18:26:40 +0100

    testing the dbt problem

--- 5-dbt-project/models/staging/stg_accounts.sql
+++ 5-dbt-project/models/staging/stg_accounts.sql
@@ -1,11 +1,12 @@
 -- models/staging/stg_accounts.sql
 
--- This model cleans and standardizes the raw account data.
--- It selects from the raw_accounts source and can be used for basic transformations.
+with source as (
+
+    select * from {{ source('on_prem_raw_data', 'raw_accounts') }}
+
+)
 
 select
     account_id,
-    -- account_holder_name,
-    opening_date as account_opening_date
-    -- processing_ts
-from {{ source('on_prem_raw_data', 'raw_accounts') }}
+    opening_date
+from source
