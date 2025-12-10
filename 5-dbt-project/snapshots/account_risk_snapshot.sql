6de7b08728905654a7a689a90c2b64d0a82aa07b
dollegit <dollegit@yahoo.com>
2025-12-10 13:38:27 +0100

    testing the dbt problem

--- 5-dbt-project/snapshots/account_risk_snapshot.sql
+++ 5-dbt-project/snapshots/account_risk_snapshot.sql
@@ -5,19 +5,14 @@
       target_schema='snapshots',
       unique_key='account_id',
       strategy='check',
-      check_cols=['risk_flag'],
+      check_cols=['risk_flag_text'],
       invalidate_hard_deletes=True
     )
 }}
 
--- The `check` strategy in snapshots computes an MD5 hash of the `check_cols`
--- to determine if a row has changed. PostgreSQL's `md5()` function does not
--- support boolean types directly, so we must cast the `risk_flag` to text
--- for the hash comparison to work.
-
 select
     account_id,
-    risk_flag::text as risk_flag -- Cast boolean to text for MD5 hashing
+    cast(risk_flag as text) as risk_flag_text
 from {{ ref('stg_risk_feed') }}
 
 {% endsnapshot %}
--- 5-dbt-project/target/compiled/snapshots/account_risk_snapshot.sql
+++ 5-dbt-project/target/compiled/snapshots/account_risk_snapshot.sql
@@ -2,13 +2,8 @@
 
 
 
--- The `check` strategy in snapshots computes an MD5 hash of the `check_cols`
--- to determine if a row has changed. PostgreSQL's `md5()` function does not
--- support boolean types directly, so we must cast the `risk_flag` to text
--- for the hash comparison to work.
-
 select
     account_id,
-    risk_flag::text as risk_flag -- Cast boolean to text for MD5 hashing
+    cast(risk_flag as text) as risk_flag_text
 from "airflow"."dev_psalmprax_staging"."stg_risk_feed"
 
