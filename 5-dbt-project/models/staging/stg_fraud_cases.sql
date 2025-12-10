5f0ce67aa966ad683ce0774de82a1c611b26f3b5
dollegit <dollegit@yahoo.com>
2025-12-10 18:58:20 +0100

    testing the dbt problem

--- 5-dbt-project/models/staging/stg_fraud_cases.sql
+++ 5-dbt-project/models/staging/stg_fraud_cases.sql
@@ -1,15 +1,12 @@
 -- models/staging/stg_fraud_cases.sql
 
 with source as (
-    select * from raw_fraud_cases
-),
 
-renamed_and_casted as (
-    select
-        "payment_id"::varchar as payment_id,
-        "fraud_type"::varchar as fraud_type,
-        "fraud_reported_date"::date as fraud_reported_date
-    from source
+    select * from {{ source('on_prem_raw_data', 'raw_fraud_cases') }}
+
 )
 
-select * from renamed_and_casted
+select
+    payment_id,
+    is_fraud as is_fraud_flag
+from source
