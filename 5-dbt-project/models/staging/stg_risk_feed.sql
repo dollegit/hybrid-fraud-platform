24687e80a592224ad26cfbfdd93da9a1951acc49
dollegit <dollegit@yahoo.com>
2025-12-10 16:26:23 +0100

    testing the dbt problem

--- 5-dbt-project/models/staging/stg_risk_feed.sql
+++ 5-dbt-project/models/staging/stg_risk_feed.sql
@@ -1,14 +1,10 @@
 -- models/staging/stg_risk_feed.sql
 
-with source as (
-    select * from raw_risk_feed
-),
-
-renamed_and_casted as (
+with renamed_and_casted as (
     select
-        "account_id"::varchar as account_id,
-        ("risk_flag"::integer = 1)::boolean as risk_flag
-    from source
+        "ACCOUNT_ID"::varchar as account_id,
+        ("RISK_FLAG"::integer = 1) as risk_flag
+    from {{ source('on_prem_raw_data', 'raw_risk_feed') }}
 )
 
 select * from renamed_and_casted
