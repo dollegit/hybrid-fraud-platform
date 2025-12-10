6c67e7c197198cdca07013cf5d45ac9fa07a1fd1
dollegit <dollegit@yahoo.com>
2025-12-10 17:09:08 +0100

    testing the dbt problem

--- 5-dbt-project/snapshots/account_risk_snapshot.sql
+++ 5-dbt-project/snapshots/account_risk_snapshot.sql
@@ -5,14 +5,14 @@
       target_schema='snapshots',
       unique_key='account_id',
       strategy='check',
-      check_cols=['account_id','risk_flag'],
+      check_cols=['account_id','stg_risk_feed'],
       invalidate_hard_deletes=True
     )
 }}
 
 select
     account_id,
-    risk_flag
+    stg_risk_feed
 from {{ ref('stg_risk_feed') }}
 
 {% endsnapshot %}
